43ed6c9be13dbc25ed16521610cdf98927baa3c0
SelimChehwane <selim_chehwane@hotmail.com>
2024-07-05 22:15:40 +0300

    Initializing Modules and Libraries

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const axios = require("axios"); //Make the HTTP Requests for Node.js
+const cheerio = require("cheerio");//Library to Parse HTML
+const readline = require("readline");//Built-in Module to Read Command Line
+const fs = require("fs");//To Create the JSON File
