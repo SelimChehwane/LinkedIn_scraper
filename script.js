16ab43184e4e67bb1319d70481d82e0baa4b412f
SelimChehwane <selim_chehwane@hotmail.com>
2024-07-05 22:38:04 +0300

    Initializinng User Prompt

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-const axios = require("axios"); //Make the HTTP Requests for Node.js
+const axios = require("axios"); //Makes the HTTP Requests for Node.js
 const cheerio = require("cheerio");//Library to Parse HTML
 const readline = require("readline");//Built-in Module to Read Command Line
 const fs = require("fs");//To Create the JSON File
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+// Reads User Input and Displays Them on the Console
+
