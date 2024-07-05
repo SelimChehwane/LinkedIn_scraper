0309c5c2243bbdfffd830cfad4e3915eef7577dd
SelimChehwane <selim_chehwane@hotmail.com>
2024-07-05 23:36:30 +0300

    Dynamicaly Adding Keyword

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const rl = readline.createInterface({
 });
 // Reads User Input and Displays Them on the Console
 
+
+rl.question("Enter the job title you want search for: ", (jobTitle) => {
+  console.log(jobTitle)
+
+  const url = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=${encodeURIComponent(jobTitle)}&location=Beirut&geoId=105606446&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=5`;
+
+ 
+  rl.close()})
+//Prompts the User for the Job Title and Inputs it into the URL as a Search Keyword
+
+ 
+
+
